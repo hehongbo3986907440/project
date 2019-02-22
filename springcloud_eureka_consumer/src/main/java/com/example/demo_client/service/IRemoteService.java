375f5e285da243583fb19c8ab9dc1e4c1e419d72
hehongbo <hehongbo3986907440@163.com>
2019-02-22 16:24:11 +0800

    修改服务的名称，不能使用下划线

--- springcloud_eureka_consumer/src/main/java/com/example/demo_client/service/IRemoteService.java
+++ springcloud_eureka_consumer/src/main/java/com/example/demo_client/service/IRemoteService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name="springcloud_eureka_producter")
+@FeignClient(name="springcloud-eureka-producter")
 public interface IRemoteService {
 	@RequestMapping(value="/producter/service")
 	public String getMess(@RequestParam(value="mes")String mes);
