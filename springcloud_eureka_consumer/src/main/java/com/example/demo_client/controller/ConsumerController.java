72458e2b8b33348f1ca5d6da0a8e7e7c4bf7852e
hehongbo <hehongbo3986907440@163.com>
2019-02-26 09:06:16 +0800

    test

--- springcloud_eureka_consumer/src/main/java/com/example/demo_client/controller/ConsumerController.java
+++ springcloud_eureka_consumer/src/main/java/com/example/demo_client/controller/ConsumerController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo_client.service.IRemoteService;
-import com.example.demo_client.service.IRemoteService2;
 
 @RestController
 @RequestMapping("/consumer")
@@ -14,14 +13,10 @@ public class ConsumerController {
 	@Autowired
 	private IRemoteService remoteService;
 	
-	@Autowired
-	private IRemoteService2 remoteService2;
-	
 	@RequestMapping("service")
 	public String publish(String mes){
-		String result = remoteService.getMess2();
+		String result = remoteService.getMess(mes);
 		System.out.println(result);
 		return result;
 	}
-	
 }
