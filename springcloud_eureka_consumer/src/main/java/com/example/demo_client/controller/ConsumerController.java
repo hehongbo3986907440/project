2c870baadd52d567f0b7f7ace83b5c85efe4761f
hehongbo <hehongbo3986907440@163.com>
2019-02-26 08:49:43 +0800

    删除test性的代码

--- springcloud_eureka_consumer/src/main/java/com/example/demo_client/controller/ConsumerController.java
+++ springcloud_eureka_consumer/src/main/java/com/example/demo_client/controller/ConsumerController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo_client.service.IRemoteService;
+import com.example.demo_client.service.IRemoteService2;
 
 @RestController
 @RequestMapping("/consumer")
@@ -13,10 +14,14 @@ public class ConsumerController {
 	@Autowired
 	private IRemoteService remoteService;
 	
+	@Autowired
+	private IRemoteService2 remoteService2;
+	
 	@RequestMapping("service")
 	public String publish(String mes){
-		String result = remoteService.getMess(mes);
+		String result = remoteService.getMess2();
 		System.out.println(result);
 		return result;
 	}
+	
 }
