8d38a91130c7bbd686e3457f63e377649b66cce3
hehongbo <hehongbo3986907440@163.com>
2019-02-26 09:04:40 +0800

    test

--- springcloud_eureka_consumer/src/main/java/com/example/demo_client/DemoClientApplication.java
+++ springcloud_eureka_consumer/src/main/java/com/example/demo_client/DemoClientApplication.java
@@ -3,9 +3,7 @@ package com.example.demo_client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.feign.FeignClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
