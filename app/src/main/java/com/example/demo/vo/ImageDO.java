4957c6bb3cbf949d08892b144bd338adca70f3b5
象征 <hehongbo3986907440@163.com>
2019-07-02 11:45:19 +0800

    增加图片数据封装类，封装测试数据

---
+++ app/src/main/java/com/example/demo/vo/ImageDO.java
@@ -0,0 +1,44 @@
+package com.example.demo.vo;
+
+public class ImageDO {
+	private int id;
+
+	private String title;
+
+	private String url;
+
+	private String desrc;
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	public void setDesrc(String desrc) {
+		this.desrc = desrc;
+	}
+
+	public String getDesrc() {
+		return this.desrc;
+	}
+
+}
